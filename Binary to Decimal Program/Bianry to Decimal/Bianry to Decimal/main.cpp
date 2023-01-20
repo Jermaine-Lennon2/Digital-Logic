60f99469b52af522c82590b6c7ff477cb1763b00
JermaineLennon <106980530+jermaine-lennon2@users.noreply.github.com>
2023-01-20 14:28:59 -0500

    bcd

---
+++ Binary to Decimal Program/Bianry to Decimal/Bianry to Decimal/main.cpp
@@ -0,0 +1,33 @@
+//C++ Program to Convert Binary Number to Decimal
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int convert(long long);
+
+int main(){
+    long long n;
+    
+    cout << "Enter a binary number: \n\n";
+    cin >> n;
+    
+    cout << n << " in binary = " << convert(n) << " in decimal \n\n";
+    
+    return 0;
+}
+
+int convert(long long n){
+    
+    int dec = 0, i = 0, rem;
+    
+    while (n!=0) {
+        rem = n % 10;
+        n /= 10;
+        dec += rem * pow(2, i);
+        ++i;
+        
+    }
+    return dec;
+}
